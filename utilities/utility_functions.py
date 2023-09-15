c9973406185aa21206292785eb0cd3b631f1da87
Tzach Agam <juj@qlik.com>
2023-09-15 23:04:35 +0300

    changed the name of the general tasks page and added doc strings

--- utilities/utility_functions.py
+++ utilities/utility_functions.py
@@ -1,12 +1,18 @@
 from selenium.common.exceptions import *
-
+"""
+Utility Functions for Qlik Replicate Automation project.
+This module contains a set of utility functions to simplify common tasks in the project. 
+These functions are designed to provide error handling and make it easier to interact with web elements files and logs.
+"""
 
 def safe_click(element):
     """ Safely clicks a web element while handling common exceptions.
-        :param element: The web element to click."""
+        :param element: The web element to click.
+        :raises NoSuchElementException: If the element is not found on the web page.
+        :raises ElementClickInterceptedException: If the click is intercepted by another element."""
 
     try:
         element.click()
     except (NoSuchElementException, ElementClickInterceptedException,
-            ElementNotInteractableException, ElementNotSelectableException,) as e:
+            ElementNotInteractableException, ElementNotSelectableException) as e:
         print(f"Error clicking element: {e}")
